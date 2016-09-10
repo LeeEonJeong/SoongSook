aaea3e24645890a0eaa6d9a1843d82dda026558f
iso <iso@172.30.1.55>
2016-09-10 13:23:48 +0900

    db

--- src/main/java/com/hanium/myapp/Reservation/ReservationDB.java
+++ src/main/java/com/hanium/myapp/Reservation/ReservationDB.java
@@ -1,5 +1,8 @@
 package com.hanium.myapp.Reservation;
 
 public class ReservationDB {
-
+	//currentUserState, userAnswerString, userKey를 넘겨받아서, 스테이트별로 저장하는거 case나누고 
+	//현재까지 저장된 디비를 스트링으로 리턴 method : getReservationConfirm();
+	
+	
 }
