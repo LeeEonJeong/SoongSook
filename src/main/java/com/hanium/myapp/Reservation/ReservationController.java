261e426c328c870259fc81fdffac73614736b464
jungmin1004 <chicolivia@gmail.com>
2016-09-10 13:23:09 +0900

    예매 디비 및 메소드 진행중

--- src/main/java/com/hanium/myapp/Reservation/ReservationController.java
+++ src/main/java/com/hanium/myapp/Reservation/ReservationController.java
@@ -6,13 +6,27 @@ public class ReservationController {
 	private KeyboardAndMessageVO keyboardAndMessageVO;
 	private int currentUserState;
 	private String userAnswerString;
+	private String userKey;
+	private ReservationDB reservationDB;
+	private final static int entireReservation = 111;
 	
-	public ReservationController (int currentUserState, String userAnswerString) {
+	
+	public ReservationController (int currentUserState, String userAnswerString, String userKey) {
 		this.currentUserState = currentUserState;
 		this.userAnswerString = userAnswerString;
+		reservationDB = new reservationDB(currentUserState, userAnswerString, userKey);
+		String reservationConfirm = reservationDB.getReservationConfirm();
+		analysisState();
 	}
 
-	
+	public void analysisState() {
+		int reservationMethod = currentUserState/10;
+		switch(reservationMethod) {
+			case entireReservation:
+				
+				break;
+		}
+	}
 	
 	public KeyboardAndMessageVO getKeyboardAndMessageVO() {
 		return keyboardAndMessageVO;
