0aa318109765183f4af6bc7feaf68f15080e908a
jungmin1004 <chicolivia@gmail.com>
2016-09-19 16:54:14 +0900

    test

--- src/main/java/com/hanium/myapp/Reservation/ReservationController.java
+++ src/main/java/com/hanium/myapp/Reservation/ReservationController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.hanium.myapp.Reservation;
 
 import com.haniumpkg.myapp.KeyboardAndMessageVO;
@@ -192,43 +191,3 @@ public class ReservationController {
 	}	
 }
 
-=======
-package com.hanium.myapp.Reservation;
-
-import com.haniumpkg.myapp.KeyboardAndMessageVO;
-
-public class ReservationController {
-	private KeyboardAndMessageVO keyboardAndMessageVO;
-	private int currentUserState;
-	private String userAnswerString;
-	private String userKey;
-	private ReservationDB reservationDB;
-	private final static int entireReservation = 111;
-	
-	
-	public ReservationController (int currentUserState, String userAnswerString, String userKey) {
-		this.currentUserState = currentUserState;
-		this.userAnswerString = userAnswerString;
-		//reservationDB = new reservationDB(currentUserState, userAnswerString, userKey);
-		//String reservationConfirm = reservationDB.getReservationConfirm();
-		analysisState();
-	}
-
-	public void analysisState() {
-		int reservationMethod = currentUserState/10;
-		switch(reservationMethod) {
-			case entireReservation:
-				break;
-		}
-	}
-	
-	public KeyboardAndMessageVO getKeyboardAndMessageVO() {
-		return keyboardAndMessageVO;
-	}
-
-	public void setKeyboardAndMessageVO(KeyboardAndMessageVO keyboardAndMessageVO) {
-		this.keyboardAndMessageVO = keyboardAndMessageVO;
-	}
-}
-
->>>>>>> branch 'master' of https://github.com/LeeEonJeong/SoongSook
